c07a8b9d9fdb612b0c2a2a144ba590097b098c7a
MariySemenenko <mashyni92@gmail.com>
2023-10-24 09:26:57 +0300

    create new page fot header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import chevronDown from '../img/header/chevronDown.svg'
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+       <header className="header">
+      <h1 className='headerTitle'>Marii Semenencko</h1>
+        <p className="headerSubtitle">
+          Front-end Developer (Java Script / HTML / CSS / React / Node.js)
+        </p>
+        <p className="headerSubtitle">https://github.com/MariySemenenko</p>
+        <a href="#Ressume" class="header-arrow">
+         
+          <img className='chevronDown' src={chevronDown} alt="Arrow up" />
+        </a>
+        </header>
     </div>
   );
 };
